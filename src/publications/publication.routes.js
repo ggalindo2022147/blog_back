97d20dbf6919fd8b2250674cc1e89e7dc64b4348
ggalindo <ggalindo-2022147@kinal.edu.gt>
2024-05-10 02:58:16 -0600

    se termino el backend aun en desarrollo por si se necesita modificaciones

--- src/publications/publication.routes.js
+++ src/publications/publication.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { validarCampos } from "../middlewares/validar-campos.js";
+import { publicationsGet, publicationsPut, publicationsGetById } from "./publication.controller";
 
 const router = Router();
 
+router.get('/publicacion', publicationsGet);
+
+router.get('/publicacion/:id', publicationsGetById);
+
+router.put('/publicacion/:id', [
+    check('id', 'No es un ID válido').isMongoId(),
+], publicationsPut);
 
 export default router;
